3a54f5f52f25bef64778fb327ebda5a390741282
Albin Baby <albinbabayme05@gmail.com>
2025-05-29 19:34:41 +0200

    field variable setup for api name

---
+++ WeatherApp.Core/LocationInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+public class LocationInfo
+{
+    public string name { get; set; }
+    public double lat { get; set; }
+    public double lon { get; set; }
+    public string country;
+    public string state;
+
+  
+}
